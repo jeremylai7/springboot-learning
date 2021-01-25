6f8eba773b2a653ba48fd4ce5e842bbc95a6e1f7
jeremylai7 <1049024592@qq.com>
2021-01-25 17:50:37 +0800

    add 发送方确认机制

--- springboot-rabbitmq/src/test/java/com/jeremy/SpringbootRabbitmqApplicationTests.java
+++ springboot-rabbitmq/src/test/java/com/jeremy/SpringbootRabbitmqApplicationTests.java
@@ -1,25 +1,38 @@
 package com.jeremy;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@Slf4j
 public class SpringbootRabbitmqApplicationTests {
 
 	@Autowired
-	private AmqpTemplate amqpTemplate;
+	private RabbitTemplate rabbitTemplate;
 
 	@Test
 	public void contextLoads() {
-		for (int i = 0; i < 10; i++) {
-			amqpTemplate.convertAndSend("myQueuess",i + " this is message");
-		}
+		rabbitTemplate.convertAndSend("myQueuess", " this is message");
+		//发送方确认机制
+		rabbitTemplate.setConfirmCallback((correlationData, ack, s) -> {
+			log.info("CorrelationData content : " + correlationData);
+			log.info("Ack status : " + ack);
+			log.info("Cause content : " + s);
+			if (ack){
+				log.info("【发送成功】");
+			}else {
+				log.info("【发送失败】:{},消息异常:{}",correlationData,s);
+			}
+		});
 	}
 
 	/**
@@ -27,12 +40,13 @@ public class SpringbootRabbitmqApplicationTests {
 	 */
 	@Test
 	public void orderContext() {
-		amqpTemplate.convertSendAndReceive("orderExchange","computer","this is comput message");
+		rabbitTemplate.convertSendAndReceive("orderExchange","computer","this is comput message");
+
 	}
 
 	@Test
 	public void fruitContext() {
-		amqpTemplate.convertAndSend("orderExchange","fruit","this is fruit message");
+		rabbitTemplate.convertAndSend("orderExchange","fruit","this is fruit message");
 	}
 
 }
