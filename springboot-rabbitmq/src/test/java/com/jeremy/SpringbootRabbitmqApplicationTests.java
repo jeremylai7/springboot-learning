17a6cbd151eb71dc6b0369c9a454c6262223000f
jeremylai7 <1049024592@qq.com>
2021-10-12 09:54:14 +0800

    删除无用的import

--- springboot-rabbitmq/src/test/java/com/jeremy/SpringbootRabbitmqApplicationTests.java
+++ springboot-rabbitmq/src/test/java/com/jeremy/SpringbootRabbitmqApplicationTests.java
@@ -1,12 +1,9 @@
 package com.jeremy;
 
 
-import com.jeremy.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
