49e30c5092c2a8b218172a838bad925e67f9bbf0
jeremylai7 <1049024592@qq.com>
2022-04-19 00:45:50 +0800

    添加 主题模式

---
+++ spring-rabbitmq/src/main/java/com/jeremy/pattern/Topic.java
@@ -0,0 +1,88 @@
+package com.jeremy.pattern;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: laizc
+ * @date: created in 2022/4/18
+ * @desc: 通配符 routing key
+ * # 一个或多个 * 一个
+ **/
+@Configuration
+@RestController
+public class Topic {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public Queue topicFirstQueue() {
+        return new Queue("topicFirstQueue");
+    }
+
+    @Bean
+    public Queue topicSecondQueue() {
+        return new Queue("topicSecondQueue");
+    }
+
+    @Bean
+    public Queue topicThirdQueue() {
+        return new Queue("topicThirdQueue");
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange("topicExchange");
+    }
+
+    @Bean
+    public Binding topicFirstBind() {
+        // first 为结尾
+        return BindingBuilder.bind(topicFirstQueue()).to(topicExchange()).with("#first#");
+    }
+
+    @Bean
+    public Binding topicSecondBind() {
+        return BindingBuilder.bind(topicSecondQueue()).to(topicExchange()).with("secondtopicBind");
+    }
+
+    @Bean
+    public Binding topicThirdBind() {
+        return BindingBuilder.bind(topicThirdQueue()).to(topicExchange()).with("thirdtopicBind");
+    }
+
+    /**
+     * 接收消息
+     */
+
+    @RabbitListener(queues = "topicFirstQueue")
+    public void topicFirstListener(String message) {
+        System.out.println("【topic first】" + message);
+    }
+
+    @RabbitListener(queues = "topicSecondQueue")
+    public void topicSecondListener(String message) {
+        System.out.println("【topic second】" + message);
+    }
+
+    @RabbitListener(queues = "topicThirdQueue")
+    public void topicThirdListener(String message) {
+        System.out.println("【topic third】" + message);
+    }
+
+    @GetMapping("/topic-first-send")
+    public String topicFirstSend(String message) {
+        rabbitTemplate.convertAndSend("topicExchange",message,"topic first ");
+        return "topic ok";
+    }
+
+
+
+}
