2c76bf22e25ffe2d54a9d2bb5a4516b8bd0c67e7
jeremylai7 <1049024592@qq.com>
2022-04-19 19:28:29 +0800

    添加路由模式

---
+++ spring-rabbitmq/src/main/java/com/jeremy/pattern/Routing.java
@@ -0,0 +1,90 @@
+package com.jeremy.pattern;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: laizc
+ * @date: created in 2022/4/18
+ * @desc: 路由模式
+ **/
+@Configuration
+@RestController
+public class Routing {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public Queue routingFirstQueue() {
+        return new Queue("routingFirstQueue");
+    }
+
+    @Bean
+    public Queue routingSecondQueue() {
+        return new Queue("routingSecondQueue");
+    }
+
+    @Bean
+    public Queue routingThirdQueue() {
+        return new Queue("routingThirdQueue");
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("routingExchange");
+    }
+
+    @Bean
+    public Binding routingFirstBind() {
+        return BindingBuilder.bind(routingFirstQueue()).to(directExchange()).with("firstRoutingBind");
+    }
+
+    @Bean
+    public Binding routingSecondBind() {
+        return BindingBuilder.bind(routingSecondQueue()).to(directExchange()).with("secondRoutingBind");
+    }
+
+    @Bean
+    public Binding routingThirdBind() {
+        return BindingBuilder.bind(routingThirdQueue()).to(directExchange()).with("thirdRoutingBind");
+    }
+
+    /**
+     * 接收消息
+     */
+
+    @RabbitListener(queues = "routingFirstQueue")
+    public void routingFirstListener(String message) {
+        System.out.println("【routing first】" + message);
+    }
+
+    @RabbitListener(queues = "routingSecondQueue")
+    public void routingSecondListener(String message) {
+        System.out.println("【routing second】" + message);
+    }
+
+    @RabbitListener(queues = "routingThirdQueue")
+    public void routingThirdListener(String message) {
+        System.out.println("【routing third】" + message);
+    }
+
+
+    @GetMapping("/routing-first")
+    public String routingFirst() {
+        // 使用不同的routingKey 转发到不同的队列
+        rabbitTemplate.convertAndSend("routingExchange","firstRoutingBind"," first routing message");
+        return "ok";
+    }
+
+
+}
