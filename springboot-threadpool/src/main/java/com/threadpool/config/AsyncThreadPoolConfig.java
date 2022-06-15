32dce4f8f77353b6e0ee86ecf56fb637b2301b6a
jeremylai7 <1049024592@qq.com>
2022-06-15 11:15:00 +0800

    add springboot thread pool

---
+++ springboot-threadpool/src/main/java/com/threadpool/config/AsyncThreadPoolConfig.java
@@ -0,0 +1,32 @@
+package com.threadpool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: laizc
+ * @Date: created in 2022-06-15
+ */
+@Configuration
+public class AsyncThreadPoolConfig {
+
+    @Bean
+    public Executor asyncThreadPool() {
+        int corePoolSize = 5;
+        int maximumPoolSize = 5;
+        long keepAliveTime = 10;
+        int queueSize = 100;
+        return new ThreadPoolExecutor(corePoolSize,
+                maximumPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,new ArrayBlockingQueue<>(queueSize),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    }
+
+}
