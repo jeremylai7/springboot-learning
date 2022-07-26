a88ea93f463a5e2ada592f4ea4d391173e74de1e
jeremylai7 <1049024592@qq.com>
2022-07-26 17:12:36 +0800

    修改创建队列

--- spring-rabbitmq/src/main/java/com/jeremy/pattern/Rpc.java
+++ spring-rabbitmq/src/main/java/com/jeremy/pattern/Rpc.java
@@ -1,10 +1,9 @@
 package com.jeremy.pattern;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +20,7 @@ public class Rpc {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
-	@Bean
-	public Queue rpcQueue() {
-		return new Queue("rpcQueue");
-	}
-
-	@RabbitListener(queues = "rpcQueue")
+	@RabbitListener(queuesToDeclare =@Queue("rpcQueue"))
 	public String rpcListener(String message) {
 		System.out.println("【rpc接收消息】" + message);
 		return "rpc 已收到" + message;
@@ -35,7 +29,7 @@ public class Rpc {
 	@GetMapping("/rpc-send")
 	public void rpcSend() {
 		Object receive = rabbitTemplate.convertSendAndReceive("rpcQueue","rpc send message");
-		System.out.println("【接收消息】" + receive);
+		System.out.println("【发送端接收消息】" + receive);
 	}
 
 
