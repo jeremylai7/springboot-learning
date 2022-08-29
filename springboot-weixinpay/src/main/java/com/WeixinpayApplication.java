52a5df487d325c1df01791b1c3574698c1f24a66
jeremylai7 <1049024592@qq.com>
2022-08-29 12:49:00 +0800

    添加微信支付

---
+++ springboot-weixinpay/src/main/java/com/WeixinpayApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinpayApplication.class, args);
+    }
+
+}
