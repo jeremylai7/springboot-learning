c4a4f9bae09421bf3a9b4042471f484d7a1cca3a
jeremylai7 <1049024592@qq.com>
2023-04-07 07:23:08 +0800

    修改文件名

--- springboot-zookeeper/src/main/java/com/Controller.java
+++ springboot-zookeeper/src/main/java/com/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @desc:
  */
 @RestController
-public class ZookeeperController {
+public class Controller {
 
     @Autowired
     private ZkApi zkApi;
