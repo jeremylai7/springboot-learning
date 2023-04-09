b815da9fe03a440ecbc47f67d0c5527db3874937
jeremylai7 <1049024592@qq.com>
2023-04-10 00:31:58 +0800

    add 秒杀

--- springboot-zookeeper/src/main/java/com/Controller.java
+++ springboot-zookeeper/src/main/java/com/Controller.java
@@ -1,5 +1,8 @@
 package com;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.config.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +19,23 @@ public class Controller {
     @Autowired
     private ZkApi zkApi;
 
+    @Autowired
+    private IService iService;
+
     @GetMapping("/create")
     public boolean create(String path,String data) {
         return zkApi.createNode(path,data);
+    }
+
+    @GetMapping("/sec-kill")
+    public String secKill() throws Exception {
+        iService.secKill();
+        return "ok";
+    }
+
+    @GetMapping("/query")
+    public String query() {
+        JSONObject json = iService.query();
+        return "订单数：" + json.getInteger("orderNum") + ",库存剩余" + json.getInteger("storeNum");
     }
 }
---
+++ springboot-zookeeper/src/main/java/com/config/IService.java
@@ -0,0 +1,50 @@
+package com.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: laizc
+ * @date: created in 2023/4/9
+ * @desc:
+ **/
+@Service
+public class IService {
+
+    private static Map<String,Integer> order = new HashMap<>();
+
+    private static Map<String,Integer> store = new HashMap<>();
+
+    static {
+        store.put("a",100);
+        order.put("a",0);
+
+    }
+
+    public void secKill() throws Exception {
+        // 扣减库存,创建订单
+        int num = store.get("a");
+        if (num > 0) {
+            store.put("a",num - 1);
+            int orderNum = order.get("a");
+            Thread.sleep(100);
+            order.put("a",orderNum + 1);
+        } else {
+            throw new Exception("库存不够");
+        }
+
+    }
+
+    public JSONObject query() {
+        JSONObject json = new JSONObject();
+        int orderNum = order.get("a");
+        int storeNum = store.get("a");
+        json.put("orderNum",orderNum);
+        json.put("storeNum",storeNum);
+        return json;
+    }
+}
