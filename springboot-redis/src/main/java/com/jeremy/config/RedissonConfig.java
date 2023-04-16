18b7e8eb03c9c598d2364664bce272527c486f7c
jeremylai7 <1049024592@qq.com>
2023-04-16 23:27:59 +0800

    redisson 分布式锁

---
+++ springboot-redis/src/main/java/com/jeremy/config/RedissonConfig.java
@@ -0,0 +1,36 @@
+package com.jeremy.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: laizc
+ * @date: created in 2023/4/16
+ * @desc:
+ **/
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${sprig.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        // 单机模式
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password);
+        return Redisson.create(config);
+    }
+}
--- springboot-redis/src/main/java/com/jeremy/controller/DistributedLockController.java
+++ springboot-redis/src/main/java/com/jeremy/controller/DistributedLockController.java
@@ -2,7 +2,10 @@ package com.jeremy.controller;
 
 import com.jeremy.exception.BusinessException;
 import com.jeremy.service.DistributedLockService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,9 @@ public class DistributedLockController {
     @Autowired
     private DistributedLockService distributedLockService;
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     /**
      * 分布式锁实现订单秒杀
      * @return
@@ -27,6 +33,21 @@ public class DistributedLockController {
     public String order() throws BusinessException {
         distributedLockService.order();
         return "ok";
+    }
+
+    @GetMapping("/redission")
+    public String redission() {
+        // 获取锁
+        RLock rLock = redissonClient.getLock("lock");
+        // 加锁
+        rLock.lock();
+
+        // 执行业务数据
+
+        // 解锁
+        rLock.unlock();
+        return "ok";
+
     }
 
 
