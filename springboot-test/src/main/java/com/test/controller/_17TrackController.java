3639bf4198bf6f17885f441535c04124debbdf3f
jeremylai7 <1049024592@qq.com>
2023-10-12 15:52:27 +0800

    接收推送通知

---
+++ springboot-test/src/main/java/com/test/controller/_17TrackController.java
@@ -0,0 +1,21 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: laizc
+ * @date: created in 2023/10/12
+ * @desc: 17track 通知
+ **/
+@RestController
+public class _17TrackController {
+
+    @PostMapping("/web-hook")
+    public String webHook(@RequestBody String body) {
+
+        return "";
+    }
+
+}
