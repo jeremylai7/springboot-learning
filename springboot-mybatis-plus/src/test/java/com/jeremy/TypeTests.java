21cf39b8425f5ca8361e745c77c2f28f96cdd3e4
jeremylai7 <1049024592@qq.com>
2024-05-27 23:55:29 +0800

    整合 mybatis plus

---
+++ springboot-mybatis-plus/src/test/java/com/jeremy/TypeTests.java
@@ -0,0 +1,30 @@
+package com.jeremy;
+
+
+import com.jeremy.dao.TypeMapper;
+import com.jeremy.model.Type;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TypeTests {
+
+	@Autowired
+	private TypeMapper typeMapper;
+
+	@Test
+	public void test() {
+		System.out.println(("----- selectAll method test ------"));
+		List<Type> userList = typeMapper.selectList(null);
+		userList.forEach(System.out::println);
+
+
+	}
+
+}
