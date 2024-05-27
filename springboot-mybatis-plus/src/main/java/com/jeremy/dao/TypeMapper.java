d8c33f53c18d5f634b2a7d8fe7cff1feea0af1e0
jeremylai7 <1049024592@qq.com>
2024-05-27 23:54:48 +0800

    整合mybatis plus

---
+++ springboot-mybatis-plus/src/main/java/com/jeremy/dao/TypeMapper.java
@@ -0,0 +1,12 @@
+package com.jeremy.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jeremy.model.Type;
+
+/**
+ * @author: laizc
+ * @date: created in 2024/5/27
+ * @desc:
+ **/
+public interface TypeMapper extends BaseMapper<Type> {
+}
---
+++ springboot-mybatis-plus/src/main/java/com/jeremy/model/Type.java
@@ -0,0 +1,16 @@
+package com.jeremy.model;
+
+import lombok.Data;
+
+/**
+ * @author: laizc
+ * @date: created in 2024/5/27
+ * @desc:
+ **/
+@Data
+public class Type {
+
+    private Integer typeId;
+
+    private String typeName;
+}
