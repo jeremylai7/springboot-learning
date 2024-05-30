66c1e6d9aa66c41994ca05025eae30660be01f96
jeremylai7 <1049024592@qq.com>
2024-05-31 00:35:27 +0800

    add elasticsearch

---
+++ springboot-elasticsearch/src/test/java/com/jeremy/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeremy;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringbootElasticsearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
