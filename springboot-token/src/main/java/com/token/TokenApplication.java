5a4ace8bb03ea7658134fdefeacf607401dcc7b0
laizc <1049024592@qq.com>
2025-09-16 14:29:20 +0800

    add sa token

---
+++ springboot-token/src/main/java/com/token/TokenApplication.java
@@ -0,0 +1,13 @@
+package com.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenApplication.class, args);
+    }
+
+}
---
+++ springboot-token/src/test/java/com/token/TokentApplicationTests.java
@@ -0,0 +1,14 @@
+package com.token;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
