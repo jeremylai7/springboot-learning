43b2ac062dfcd999362e5478f5801627c8e7db08
laizc <1049024592@qq.com>
2026-03-30 10:25:22 +0800

    添加ddd 基础框架

---
+++ springboot-ddd/src/test/java/ddd/DddApplicationTests.java
@@ -0,0 +1,13 @@
+package ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
