e9890a96314087bd8ccb815de85d23e1218e7579
laizc <1049024592@qq.com>
2026-05-09 18:04:48 +0800

    升级 jackson 版本

--- springboot-token/src/main/java/com/token/TokenApplication.java
+++ springboot-token/src/main/java/com/token/TokenApplication.java
@@ -1,5 +1,6 @@
 package com.token;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class TokenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TokenApplication.class, args);
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 
 }
--- springboot-token/src/test/java/com/token/TokentApplicationTests.java
+++ springboot-token/src/test/java/com/token/TokentApplicationTests.java
@@ -2,13 +2,16 @@ package com.token;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class TokentApplicationTests {
+public class TokentApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
